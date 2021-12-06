e01c2e7812633bce3e5acd68e0a1a14befdef717
Weslley Oliveira <weslleyoliveira258@gmail.com>
2021-12-06 18:37:13 -0300

    feat: create  list repositories

--- src/index.js
+++ src/index.js
@@ -23,7 +23,9 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
@@ -48,7 +50,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
